83a834ce7ea72b479f14f9071c1a551c733bf334
Anna Komkova <ann.komkova98@gmail.com>
2025-10-05 02:26:49 +0300

    new style, add structure

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -9,14 +9,8 @@ function createPage(template, filename) {
 
 const htmlPages = [
   createPage('./src/index.html', './index.html'),
-  createPage('./src/pages/articles.html', './pages/articles.html'),
-  createPage('./src/pages/dictionary.html', './pages/dictionary.html'),
-  createPage('./src/pages/tests.html', './pages/tests.html'),
-  createPage('./src/pages/tests/test1.html', './pages/tests/test1.html'),
-  createPage(
-    './src/pages/articles/article1.html',
-    './pages/articles/article1.html'
-  )
+  createPage('./src/pages/L-SH-2025.html', './pages/L-SH-2025.html'),
+  createPage('./src/pages/L-SH-2024.html', './pages/L-SH-2024.html')
 ]
 
 module.exports = htmlPages
